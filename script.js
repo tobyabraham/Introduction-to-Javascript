1824437f5acfd18abccdf1c795302d0c15d55808
Toby <32206025+tobyabraham@users.noreply.github.com>
2022-06-24 13:26:33 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+var fname = "Bolajoko Oluwatobi Abraham";
+var height = "183cm (6.0 ft)";
+var country = "Nigeria";
+var info = `Name: ${fname} <br> Height: ${height} <br> Country: ${country}`;
+
+document.write(info);
